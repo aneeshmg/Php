9bd1e3f3aebf969d5819a211b4792a13bbe7b155
aneeshmg <aneesh.mg2007@gmail.com>
2014-01-27 00:56:20 +0530

    loginsystem updated

--- LoginSystem/login.php
+++ LoginSystem/login.php
@@ -3,8 +3,15 @@
 if(isset($_POST['uname']) && isset($_POST['pass'])) {
   $username = $_POST['uname'];
   $password = $_POST['pass'];
+  login($username, $password);
 }
 else
   header("Location: index.php");
 
 echo $username . $password;
+
+function login($u, $p) {
+  mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Unable to connect to database");
+  mysql_select_db(DB_NAME) or die("Unable to select database");
+  echo "Db selected";
+}
